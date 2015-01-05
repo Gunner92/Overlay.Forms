4745e51f38353e3fc597fc57f0cebe809999f7d0
Gunner92 <burak.guner@outlook.com>
2015-01-05 13:07:06 +0200

    MyCode
    MyCode

---
+++ OverlayTest/OverlayTest/App.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace OverlayTest
+{
+	public class App
+	{
+		public static NavigationPage navMy;
+		public static Page GetMainPage ()
+		{	
+			navMy = new NavigationPage (new MyPage ());
+			return navMy;
+		}
+	}
+}
+
---
+++ OverlayTest/OverlayTest/MyPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Overlay;
+using System.Threading.Tasks;
+
+namespace OverlayTest
+{	
+	public partial class MyPage : ContentPage
+	{	
+		public double HeightX;
+		public double WidthX;
+		public LoadingOverlay loy;
+		public MyPage ()
+		{
+			InitializeComponent ();
+			MyButton.Clicked+= async(object sender, EventArgs e) => {
+				loy= new LoadingOverlay(MyStack,"Loading...",(int)HeightX,(int)WidthX,this);
+				loy.ShowOverlay(this);
+				MyStack.HeightRequest=HeightX;
+				MyStack.WidthRequest=WidthX;
+				App.navMy.PushAsync(new MyPage());
+
+			};
+		}
+		protected override void OnSizeAllocated (double width, double height)
+		{
+			HeightX = height;
+			WidthX = width;
+			base.OnSizeAllocated (width, height);
+		}
+		protected override void OnDisappearing ()
+		{
+			if (loy!=null) {
+
+				loy.HideOverlay(this);
+			}
+			base.OnDisappearing ();
+		}
+	}
+}
+
